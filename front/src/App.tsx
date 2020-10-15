4efabffe4eda798261a522ecab5ea84a4a7cb21a
EC2 Default User <ec2-user@ip-172-31-7-79.us-east-2.compute.internal>
2020-10-15 09:40:40 +0000

    First project

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/globa.css';
+//import 'leaflet/dist/leaflet.css';
+import Routes from './routes';
+
+function App() {
+  return (
+	<Routes />
+  );
+}
+
+export default App;
